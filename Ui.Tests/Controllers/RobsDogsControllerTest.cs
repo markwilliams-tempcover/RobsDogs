59a70a064258708a82ee4a79645b160a3a504c9f
Alexandru Hapco <alexandruhapco@gmail.com>
2020-04-30 21:39:21 +0100

    Controller and VMMapper tests created

--- Ui.Tests/Controllers/RobsDogsControllerTest.cs
+++ Ui.Tests/Controllers/RobsDogsControllerTest.cs
@@ -1,7 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Ui.Controllers;
+using Ui.Entities;
+using Ui.Models;
 using Ui.ViewModelMappers;
 
 namespace Ui.Tests.Controllers
@@ -13,6 +17,16 @@ namespace Ui.Tests.Controllers
 		public void Index()
 		{
             var dogOwnerViewModelMapper = new Mock<IDogOwnerViewModelMapper>();
+            var dogOwner = new DogOwner() { OwnerName = "Qwe", DogNameList = new List<string> { "q", "w", "e" } };
+            var dogOwners = new List<DogOwner> { dogOwner };
+            var dogOwnerListViewModel = new DogOwnerListViewModel {
+                DogOwnerViewModels = dogOwners.Select(e => new DogOwnerViewModel {
+                    OwnerName = e.OwnerName,
+                    DogNames = e.DogNameList
+                }).ToList()
+            };
+
+            dogOwnerViewModelMapper.Setup(x => x.GetAllDogOwners()).Returns(dogOwnerListViewModel);
             //Arrange
             RobsDogsController controller = new RobsDogsController(dogOwnerViewModelMapper.Object);
 
@@ -21,9 +35,9 @@ namespace Ui.Tests.Controllers
 
             //Assert
             Assert.IsNotNull(result);
+            Assert.AreEqual(dogOwnerListViewModel, result.Model);
             //Should be testing / verifying call to GetAllDogOwners and subsequent methods down the stack.
             //Moq is installed to help you.
-
         }
 	}
 }
---
+++ Ui.Tests/ViewModelMapper/DogOwnerViewModelMapperTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Ui.Entities;
+using Ui.Models;
+using Ui.Services;
+using Ui.ViewModelMappers;
+
+namespace Ui.Tests.ViewModelMapper {
+    [TestClass]
+    public class DogOwnerViewModelMapperTest {
+
+        [TestMethod]
+        public void GetAllDogOwners_Should_returnDogOwnerListVM_When_serviceReturnDogOwnersList() {
+
+            //Arrange
+            var dogOwnerService = new Mock<IDogOwnerService>();
+            var dogOwner = new DogOwner() { OwnerName = "Qwe", DogNameList = new List<string> { "q", "w", "e" } };
+            var dogOwners = new List<DogOwner> { dogOwner };
+            dogOwnerService.Setup(x => x.GetAllDogOwners()).Returns(dogOwners);
+
+            var dogOwnerViewModelMapper = new DogOwnerViewModelMapper(dogOwnerService.Object);
+
+            //Act
+            var result = dogOwnerViewModelMapper.GetAllDogOwners();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
